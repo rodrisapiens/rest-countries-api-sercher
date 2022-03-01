705ea74d13c051357de43a86009642a9440386f0
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-03-01 06:18:40 -0300

    finished

--- src/components/MainDashBoard.js
+++ src/components/MainDashBoard.js
@@ -64,28 +64,28 @@ function MainDashBoard({ mode }) {
                     initialInfo[0] === undefined ? null : name === "" ? region === "" ?
                         <>
                             <Link to={"/SpecializedPage"} state={{info:initialInfo[random.current[0]]}}>
-                                <CountryBox key={initialInfo[random.current[0]].name.official} flag={initialInfo[random.current[0]].flags.svg} name={initialInfo[random.current[0]].name.common} population={numberWithCommas(initialInfo[random.current[0]].population)} region={initialInfo[random.current[0]].region} capital={initialInfo[random.current[0]].capital} mode={mode} />
+                                <CountryBox key={initialInfo[random.current[0]].name.official} flag={initialInfo[random.current[0]].flags.png} name={initialInfo[random.current[0]].name.common} population={numberWithCommas(initialInfo[random.current[0]].population)} region={initialInfo[random.current[0]].region} capital={initialInfo[random.current[0]].capital} mode={mode} />
                             </Link>
                             <Link to={"/SpecializedPage"} state={{info:initialInfo[random.current[1]]}}>
-                                <CountryBox key={initialInfo[random.current[1]].name.official} flag={initialInfo[random.current[1]].flags.svg} name={initialInfo[random.current[1]].name.common} population={numberWithCommas(initialInfo[random.current[1]].population)} region={initialInfo[random.current[1]].region} capital={initialInfo[random.current[1]].capital} mode={mode} />
+                                <CountryBox key={initialInfo[random.current[1]].name.official} flag={initialInfo[random.current[1]].flags.png} name={initialInfo[random.current[1]].name.common} population={numberWithCommas(initialInfo[random.current[1]].population)} region={initialInfo[random.current[1]].region} capital={initialInfo[random.current[1]].capital} mode={mode} />
                             </Link>
                             <Link to={"/SpecializedPage"} state={{info:initialInfo[random.current[2]]}}>
-                                <CountryBox key={initialInfo[random.current[2]].name.official} flag={initialInfo[random.current[2]].flags.svg} name={initialInfo[random.current[2]].name.common} population={numberWithCommas(initialInfo[random.current[2]].population)} region={initialInfo[random.current[2]].region} capital={initialInfo[random.current[2]].capital} mode={mode} />
+                                <CountryBox key={initialInfo[random.current[2]].name.official} flag={initialInfo[random.current[2]].flags.png} name={initialInfo[random.current[2]].name.common} population={numberWithCommas(initialInfo[random.current[2]].population)} region={initialInfo[random.current[2]].region} capital={initialInfo[random.current[2]].capital} mode={mode} />
                             </Link>
                             <Link to={"/SpecializedPage"} state={{info:initialInfo[random.current[3]]}}>
-                                <CountryBox key={initialInfo[random.current[3]].name.official} flag={initialInfo[random.current[3]].flags.svg} name={initialInfo[random.current[3]].name.common} population={numberWithCommas(initialInfo[random.current[3]].population)} region={initialInfo[random.current[3]].region} capital={initialInfo[random.current[3]].capital} mode={mode} />
+                                <CountryBox key={initialInfo[random.current[3]].name.official} flag={initialInfo[random.current[3]].flags.png} name={initialInfo[random.current[3]].name.common} population={numberWithCommas(initialInfo[random.current[3]].population)} region={initialInfo[random.current[3]].region} capital={initialInfo[random.current[3]].capital} mode={mode} />
                             </Link>
                             <Link to={"/SpecializedPage"} state={{info:initialInfo[random.current[4]]}}>
-                                <CountryBox key={initialInfo[random.current[4]].name.official} flag={initialInfo[random.current[4]].flags.svg} name={initialInfo[random.current[4]].name.common} population={numberWithCommas(initialInfo[random.current[4]].population)} region={initialInfo[random.current[4]].region} capital={initialInfo[random.current[4]].capital} mode={mode} />
+                                <CountryBox key={initialInfo[random.current[4]].name.official} flag={initialInfo[random.current[4]].flags.png} name={initialInfo[random.current[4]].name.common} population={numberWithCommas(initialInfo[random.current[4]].population)} region={initialInfo[random.current[4]].region} capital={initialInfo[random.current[4]].capital} mode={mode} />
                             </Link>
                             <Link to={"/SpecializedPage"} state={{info:initialInfo[random.current[5]]}}>
-                                <CountryBox key={initialInfo[random.current[5]].name.official} flag={initialInfo[random.current[5]].flags.svg} name={initialInfo[random.current[5]].name.common} population={numberWithCommas(initialInfo[random.current[5]].population)} region={initialInfo[random.current[5]].region} capital={initialInfo[random.current[5]].capital} mode={mode} />
+                                <CountryBox key={initialInfo[random.current[5]].name.official} flag={initialInfo[random.current[5]].flags.png} name={initialInfo[random.current[5]].name.common} population={numberWithCommas(initialInfo[random.current[5]].population)} region={initialInfo[random.current[5]].region} capital={initialInfo[random.current[5]].capital} mode={mode} />
                             </Link>
                             <Link to={"/SpecializedPage"} state={{info:initialInfo[random.current[6]]}}>
-                                <CountryBox key={initialInfo[random.current[6]].name.official} flag={initialInfo[random.current[6]].flags.svg} name={initialInfo[random.current[6]].name.common} population={numberWithCommas(initialInfo[random.current[6]].population)} region={initialInfo[random.current[6]].region} capital={initialInfo[random.current[6]].capital} mode={mode} />
+                                <CountryBox key={initialInfo[random.current[6]].name.official} flag={initialInfo[random.current[6]].flags.png} name={initialInfo[random.current[6]].name.common} population={numberWithCommas(initialInfo[random.current[6]].population)} region={initialInfo[random.current[6]].region} capital={initialInfo[random.current[6]].capital} mode={mode} />
                             </Link>
                             <Link to={"/SpecializedPage"} state={{info:initialInfo[random.current[7]]}}>
-                                <CountryBox key={initialInfo[random.current[7]].name.official} flag={initialInfo[random.current[7]].flags.svg} name={initialInfo[random.current[7]].name.common} population={numberWithCommas(initialInfo[random.current[7]].population)} region={initialInfo[random.current[7]].region} capital={initialInfo[random.current[7]].capital} mode={mode} />
+                                <CountryBox key={initialInfo[random.current[7]].name.official} flag={initialInfo[random.current[7]].flags.png} name={initialInfo[random.current[7]].name.common} population={numberWithCommas(initialInfo[random.current[7]].population)} region={initialInfo[random.current[7]].region} capital={initialInfo[random.current[7]].capital} mode={mode} />
                             </Link>
                         </> : null : null
                 }
@@ -96,11 +96,14 @@ function MainDashBoard({ mode }) {
                         </Link>
                     }) : null
                 }
-                {data[0] === undefined ? "not found, keep writing..." : data.map((country) => {
+                {data[0] === undefined ?null : data.map((country) => {
                     return <Link to={"/SpecializedPage"} state={{info:country}}>
                     <CountryBox key={country.name.official} flag={country.flags.svg} name={country.name.common} population={numberWithCommas(country.population)} region={country.region} capital={country.capital} mode={mode} />
                     </Link>
                 })
+                }
+                {
+                    data[0]&&regionData[0]&&initialInfo[0]===undefined?"loading...":null
                 }
             </div>
         </>
--- src/components/SpecializedPage.js
+++ src/components/SpecializedPage.js
@@ -1,45 +1,63 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import { useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 import "../styles/specializedPage.css"
-function SpecializedPage({mode}) {
-    const location = useLocation()
-    const { info } = location.state
-    return (
+function SpecializedPage({ mode }) {
+    const location = useLocation();
+    const { info } = location.state;
+    function numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    }
 
-        <div className='specializedPage'>
-            <Link to={"/"}>
-                <button className={mode?"ligth back":"dark back"}>Back</button>
-            </Link>
+    return (<>
+        <Link to={"/"}>
+            <button className={mode ? "ligth back" : "dark back"}>
+                <FontAwesomeIcon icon={faArrowLeftLong} />
+                Back
+            </button>
+        </Link>
+        <div className={mode ? "ligth specializedPage spe" : "dark specializedPage spe"}>
+
             <img src={info.flags.svg} alt="" className="bigFlag" />
-            <h1>{info.name.common}</h1>
-            <div className="firstInfos">
-                <p className="info">Native Name:{Object.entries(info.name.nativeName).map((obj)=>
-                {
-                    return <><span className='key'>{obj[0]}</span>:{obj[1].common}</>;
-                
+            <div className="secondHalf">
+                <div className="firstOfSecond">
+                <h1>{info.name.common}</h1>
+                <div className="firstInfos">
+                    <div className="info"><b>Native Name</b>:{Object.entries(info.name.nativeName).map((obj) => {
+                        return <p className="info"key={obj[0]}>{obj[0]}:{obj[1].common}</p>;
 
-                })}</p>
-                <p className="info">Population:{info.population}</p>
-                <p className="info">Region:{info.region}</p>
-                <p className="info">Sub Region:{info.subRegion}</p>
-                <p className="info">Capital:{info.capital}</p>
-            </div>
-            <div className="secondInfos">
-                <p className="info">Top Level Domain:{info.tld[0]}</p>
-                <p className="info">Currencies:{info.currencies[0]}</p>
 
-                <p className="info">Languages:{Object.entries(info.languages).map((language)=>
-                {
-                    return language+"."
-                }) }</p>
-                <h4>Border countries</h4>
-                
+                    })}</div>
+                    <p className="info"><b>Population</b>:{numberWithCommas(info.population)}</p>
+                    <p className="info"><b>Region</b>:{info.region}</p>
+                    <p className="info"><b>Sub Region</b>:{info.subregion}</p>
+                    <p className="info"><b>Capital</b>:{info.capital}</p>
+                </div>
+                </div>
+                <div className="secondInfos">
+                    <p className="info"><b>Top Level Domain</b>:{info.tld[0]}</p>
+                    <div className="info"><b>Currencies</b>:{info.currencies===undefined?null:Object.entries(info.currencies).map((currencie) => {
+                        return <p className="info" key={currencie[1].name}>{currencie[1].name}</p>
+                    })}</div>
 
+                    <p className="info"><b>Languages</b>:{Object.entries(info.languages).map((language) => {
+                        return language[1] + "."
+                    })}</p>
+                </div>
+                <h4>Border countries:</h4>
+                <div className="borders">
+                    {
+                        info.borders === undefined ? "not" :
+                            Object.entries(info.borders).map((border) => {
+                                return <div key={border} className={mode ? 'ligth borderBox' : "dark borderBox"}>{border[1]}</div>
+                            })}
+                </div>
             </div>
         </div>
 
-    )
+    </>)
 }
 
 export default SpecializedPage
